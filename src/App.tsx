919e1d753f8c5487f93ce76fd62313c11a49aa6d
Tapiwanashe Mlambo <tapiwamla@gmail.com>
2025-08-19 05:46:12 +0300

    v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -209,7 +209,7 @@ function App() {
           </div>
           
           {/* Portfolio Grid */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-8">
             {filteredItems.map((item) => (
               <a
                 key={item.id}
